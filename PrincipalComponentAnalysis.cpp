7c596249153f21cfb469163c94b6111ccc0bbdea
Andrea Zelaya <agzelaya.f@gmail.com>
2025-02-16 23:24:05 -0600

    calculo de valores y vectores propios

--- PrincipalComponentAnalysis.cpp
+++ PrincipalComponentAnalysis.cpp
@@ -4,8 +4,24 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <iterator>
+#include <Eigen/Eigenvalues>
 
 using namespace std;
+using namespace Eigen;
+
+void imprimirMatriz(const vector<vector<double>>& matriz) {
+    int row = matriz.size();
+    int col = matriz[0].size();
+
+
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cout << matriz[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
 
 vector<vector<double>> leerCSV(const string& nombreArchivo) {
     ifstream archivo(nombreArchivo);
@@ -122,29 +138,83 @@ vector<vector<double>> correlaciones(const vector<vector<double>>& matriz) {
     return matrizCorrelacion;
 }
 
-void imprimirMatriz(const vector<vector<double>>& matriz) {
-    int row = matriz.size();
-    int col = matriz[0].size();
+pair<vector<double>, vector<vector<double>>> calcularValoresVectoresPropios(const vector<vector<double>>& matriz) {
+    int row = matriz.size();  
 
+    // convertir de tipo vector a MatrixXd
+    MatrixXd mat(row, row);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < row; j++) {
+            mat(i, j) = matriz[i][j];
+        }
+    }
+
+    SelfAdjointEigenSolver<MatrixXd> solver(mat);
+
+    VectorXd valoresPropios = solver.eigenvalues();  
+    MatrixXd vectoresPropios = solver.eigenvectors();
+
+
+    VectorXd valoresOrdenados = valoresPropios.reverse();
+    vector<double> valoresOV;
+    for (int i = 0; i < row; i++) {
+        valoresOV.push_back(valoresOrdenados(i));
+    }
+
+    vector<vector<double>> vectoresVector(row, vector<double>(row));
 
     for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
-            cout << matriz[i][j] << "\t";
+        for (int j = 0; j < row; j++) {
+            vectoresVector[i][j] = vectoresPropios(i,j);
         }
-        cout << endl;
     }
+    
+    //ordenar vectores propios de mayor a menor
+    vector<vector<double>> vectoresOrdenados;
+    vector<double> temp;
+
+    for (int i = 0; i < row; i++) {
+        for (int j = row-1; j >= 0; j--) {
+            temp.push_back(vectoresVector[i][j]);
+        }
+        vectoresOrdenados.push_back(temp);
+        temp.clear();
+    }
+
+    return{valoresOV, vectoresOrdenados};
 }
 
+
+
 int main(){
+
+    cout << " == Matriz original ==" << endl;
     imprimirMatriz(leerCSV("EjemploEstudiantes.csv"));
     cout << endl << endl;
 
     vector<vector<double>> estandarizada = estandarizar(leerCSV("EjemploEstudiantes.csv"));
-
+    cout << " == Matriz estandarizada ==" << endl;
     imprimirMatriz(estandarizada);
-
     cout << endl << endl;
 
-    imprimirMatriz(correlaciones(estandarizada));
+    cout << " == Matriz de correlaciones/covarianzas ==" << endl;
+    vector<vector<double>> correlacion = correlaciones(estandarizada);
+    imprimirMatriz(correlacion);
+    cout << endl << endl;
+
+    pair<vector<double>, vector<vector<double>>> resultados = calcularValoresVectoresPropios(correlacion);
+
+    vector<double> autovalores = resultados.first;
+    vector<vector<double>> autovectores = resultados.second;
+
+    cout << " == Valores propios ==" << endl;
+    for (int i = 0; i < autovalores.size(); i++) {
+        cout << autovalores[i] << " ";
+    }
+ 
+    cout << endl << endl;
+
+    cout << " == Vectores propios ==" << endl;
+    imprimirMatriz(autovectores);
 }
 
